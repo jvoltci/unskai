a7d52461ac6142421664dfe8f8a89f394b5c6aa4
jvoltci <jvoltci@gmail.com>
2022-05-22 16:41:29 +0530

    Initiation

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 
 app.get("/", (req, res) => res.send("It is Live now!"));
